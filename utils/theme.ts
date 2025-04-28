1b93ae994772463c56b6b54830713a0d0a590067
prabhatsn1 <prabhatsn1@gmail.com>
2025-04-28 17:03:33 +0530

    theme change

--- utils/theme.ts
+++ utils/theme.ts
@@ -1,14 +1,10 @@
 import { createTheme } from "@mui/material/styles";
 
-declare module "@mui/material/styles" {
-
-
 const theme = createTheme({
   palette: {
     primary: { main: "#2CA158" },
     secondary: { main: "#2CA158" },
     background: { default: "#222", paper: "#333" },
-   
   },
   typography: {
     fontFamily: "Georgia",
