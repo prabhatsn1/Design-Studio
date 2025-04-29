a06c388823493ea832e670478f9e5a88624dcf54
prabhatsn1 <prabhatsn1@gmail.com>
2025-04-29 09:43:17 +0530

    In between

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,7 +1,29 @@
 "use client";
 
-import MeetTheArtisans from "@/components/MeetTheArtisans";
+import { Stack } from "@mui/material";
 
 export default function TestPage() {
-  return <MeetTheArtisans />;
+  return (
+    <Stack
+      id="pp"
+      bgcolor="red"
+      sx={{
+        width: 250,
+        height: 200,
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Stack
+        bgcolor="blue"
+        sx={{ width: 100, height: 100, zIndex: 100 }}
+      ></Stack>
+      <Stack
+        bgcolor="green"
+        sx={{ width: 100, height: 100, mt: 6, ml: -2 }}
+      ></Stack>
+    </Stack>
+  );
 }
